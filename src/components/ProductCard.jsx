b62ef64845ce94f4193231f49b87f21011a1d942
osama <osamagamal1611@gmail.com>
2025-01-31 15:11:55 +0200

    ...

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -24,11 +24,13 @@ function ProductCard() {
 
   return (
     <div className="px-4 md:px-8">
-      <h3 className="text-2xl font-bold leading-9 text-gray-800">
+      <h3 className="text-2xl font-bold leading-9 text-gray-800 text-center mb-8">
         Choose your flower
       </h3>
-      <hr className="w-[90px] h-[5px] bg-red-400 border-none mt-2 mb-10" />
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 sm:gap-8 md:gap-10 place-items-center">
+      <hr className="w-[90px] h-[5px] bg-red-400 border-none mt-2 mb-10 mx-auto" />
+
+      {/* عرض المنتجات في عمودين فقط على شاشات الموبايل مع مسافة بينهم */}
+      <div className="grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 place-items-center sm:gap-8 md:gap-10">
         {flowers.map((item) => {
           if (
             selectedCategory === "all" ||
@@ -36,7 +38,7 @@ function ProductCard() {
           ) {
             return (
               <div
-                className="w-full sm:w-[160px] md:w-[180px] lg:w-[200px] p-4"
+                className="w-full sm:w-[160px] md:w-[180px] lg:w-[200px] p-4 sm:px-4 sm:py-6 md:p-4"
                 key={item.id}
               >
                 <ProductItem
